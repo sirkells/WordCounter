323c08f53622f4058d91a91d6b02a76f0c043086
Kells <Kelechi_igbokwe@yahoo.com>
2018-05-15 07:52:35 +0200

    created and added templates

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, 'home.html')
+
+def eggs(request):
+    return HttpResponse()

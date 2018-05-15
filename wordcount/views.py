2c5cd92d51803f0bddfaec6453885ed5e8717c0a
Kells <Kelechi_igbokwe@yahoo.com>
2018-05-15 10:25:57 +0200

    added loop to insert and increment words in the word dictionary

--- wordcount/views.py
+++ wordcount/views.py
@@ -9,6 +9,14 @@ def count(request):
     text = request.GET['fulltext'] #get request to fetch the inputed text(fulltext) from the form in home.html
     wordlist = text.split() #splits full texts into words
     lengthOfWords = len(wordlist) #counts the no of words
-    print(lengthOfWords)
-    return render(request, 'count.html', {'enteredtext': text, 'noOfWords': lengthOfWords}) 
+    wordDictionary = {}
+    for word in wordDictionary:
+        #if the word is already in dict, increment the value
+        if word in wordDictionary:
+            word += 1
+        else:
+            #if word isnt in dict add word and give it a value 1
+            wordDictionary[word] = 1
+  
+    return render(request, 'count.html', {'enteredtext': text, 'noOfWords': lengthOfWords, 'wordDict': wordDictionary}) 
     #{'enteredtext':text} is a dictionary that contains the get request text

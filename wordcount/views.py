b8f721e5a94474e5e7493b4eb22c0fbbb42b55a9
Kells <Kelechi_igbokwe@yahoo.com>
2018-05-15 10:52:34 +0200

    added method for the number of times a word appears

--- wordcount/views.py
+++ wordcount/views.py
@@ -10,13 +10,13 @@ def count(request):
     wordlist = text.split() #splits full texts into words
     lengthOfWords = len(wordlist) #counts the no of words
     wordDictionary = {}
-    for word in wordDictionary:
+    for word in wordlist:
         #if the word is already in dict, increment the value
         if word in wordDictionary:
-            word += 1
+            wordDictionary[word] += 1
         else:
             #if word isnt in dict add word and give it a value 1
             wordDictionary[word] = 1
   
-    return render(request, 'count.html', {'enteredtext': text, 'noOfWords': lengthOfWords, 'wordDict': wordDictionary}) 
+    return render(request, 'count.html', {'enteredtext': text, 'noOfWords': lengthOfWords, 'wordDict': wordDictionary.items()}) 
     #{'enteredtext':text} is a dictionary that contains the get request text
